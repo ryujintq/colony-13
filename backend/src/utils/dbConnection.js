1642a9755e3adc459bd557f03887d0e49265d5a8
Tariq Al Nabhani <tariqalnabhani@hotmail.com>
2021-10-18 11:02:50 +0100

    removed unnecessary code from connectDB

--- backend/src/utils/dbConnection.js
+++ backend/src/utils/dbConnection.js
@@ -1,9 +1,5 @@
 import mongoose from "mongoose"
 
-// const url = process.env.NODE_ENV === "test"
-//     ? process.env.DB_TEST_URL
-//     : process.env.DB_URL
-
 const connectDB = async () => {
     await mongoose.connect(
         process.env.DB_URL_LOCAL, {
@@ -16,10 +12,6 @@ const connectDB = async () => {
 
 const closeDB = () => {
     mongoose.disconnect()
-}
-
-if (process.env.NODE_ENV !== 'test') {
-    mongoose.connection.on("open", () => console.log("connected to database"))
 }
 
 export { connectDB, closeDB }
